8fd1e497bf70c20c8365617fcd2e1929447c1324
Denzel Manangu <93459468+Dmanangu@users.noreply.github.com>
2021-12-21 12:35:33 +0800

    navbar created

---
+++ utils/style.js
@@ -0,0 +1,62 @@
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  navbar: {
+    backgroundColor: "#0f7db0",
+    "& a": {
+      color: "#ffffff",
+      marginLeft: 10,
+    },
+  },
+  brand: {
+    fontWeight: "bold",
+    fontSize: " 1.5rem",
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  sizePlus: {
+    fontSize: "20px",
+  },
+  main: {
+    minHeight: "80vh",
+    minWidth: "80vw",
+  },
+  footer: {
+    textAlign: "center",
+  },
+  secondtext: {
+    fontSize: "23px",
+  },
+  section: {
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  addButton: {
+    primary: {
+      main: "#2596be", //changed color to lighter blue
+    },
+    secondary: {
+      main: "#2596be",
+    },
+  },
+  form: {
+    width: "100%",
+    maxWidth: 800,
+    margin: "0 auto",
+  },
+  navbarButton: {
+    color: "#ffffff",
+    textTransform: "initial",
+  },
+  transparentBackground: {
+    backgroundColor: "transparent",
+  },
+  error: {
+    color: "#f04040",
+  },
+  fullWidth: {
+    width: "100%",
+  },
+});
+export default useStyles;
